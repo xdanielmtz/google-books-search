20e33927aae283051022a27dd062ef71f2b99d13
Dani JM <xdanielmtz@gmail.com>
2021-02-03 14:10:40 -0500

    added a state hook for the search page. also installed axios to make the api call

--- client/src/components/SearchForm/SearchForm.jsx
+++ client/src/components/SearchForm/SearchForm.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchForm = () => {
+  const [search, setSearch] = useState("");
+
+ const handleFormSubmit = (e) => {
+   e.preventDefault();
+   
+ } 
+ 
+ 
   return (
-      <div className="row">
-        <form className="col s12">
-          <div className="row">
-            <div className="input-field col s12">
-              <input placeholder="Search for a Book" id="search" type="search" className="validate" />
-              <label htmlFor="email"></label>
-            </div>
+    <div className="row">
+      <form className="col s12" onSubmit={handleFormSubmit}>
+        <div className="row">
+          <div className="input-field col s12">
+            <input
+              placeholder="Search for a Book"
+              id="search"
+              type="text"
+              className="validate"
+              name="search"
+              value={search}
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+            />
+            <label htmlFor="email"></label>
           </div>
-          <button className="waves-effect waves-light btn">
-            Search
-          </button>
-        </form>
-      </div>
+        </div>
+        <button className="waves-effect waves-light btn">Search</button>
+      </form>
+    </div>
   );
 };
 
