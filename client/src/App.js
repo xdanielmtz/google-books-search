193222b7c094127d1cb2abdb7735e6c155df4693
Dani JM <xdanielmtz@gmail.com>
2021-02-03 13:44:13 -0500

    added a single input on the form and added a button

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 const { default: Banner } = require("./components/Banner/Banner");
 const { default: Navbar } = require("./components/Navbar/Navbar");
+const { default: SearchForm } = require("./components/SearchForm/SearchForm");
 
 function App() {
   return (
     <>
     <Navbar/>
     <Banner/>
+    <SearchForm/>
     </>
   );
 }
